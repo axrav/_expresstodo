f7ed682953d1d7c386e335a9cbcb59f620f852f5
Aarav Arora <aaravarora.works@gmail.com>
2022-11-08 23:56:37 +0530

    feat : exceptions

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,25 +1,46 @@
 const Task = require("../db/schema");
 // create task by mongoose
 const CreateTask = async (task) => {
-    const newTask = new Task(task);
-    await newTask.save();
-    return newTask;
+    try {
+        const newTask = await Task.create(task);
+        return newTask;
+    }
+    catch (err) {
+        return err;
+    }
 
 }
 // list tasks by mongoose
 const ListTasks = async () => {
-    const tasks = await Task.find();
-    return tasks;
+    try {
+        const tasks = await Task.find();
+        return tasks;
+    }
+    catch (err) {
+        return err;
+    }
 }
 // update task by id and mongoose
 const UpdateTask = async (id, task) => {
-    const updatedTask = await Task.findByIdAndUpdate(id, task);
-    return updatedTask;
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(id, task, { new: true });
+        return updatedTask;
+    }
+    catch (err) {
+        return err;
+    }
 }
 // delete task by id and mongoose
 const DeleteTask = async (id) => {
-    const deletedTask = await Task.findByIdAndDelete(id);
-    return deletedTask;
+    try {
+
+        const deletedTask = await Task.findByIdAndDelete(id);
+        return deletedTask;
+    }
+    catch (err) {
+        return err;
+    }
+    
 }
 
 module.exports = {
